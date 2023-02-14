6793c229a5b08925a79ebb53d0d192a2103cbb37
Ilya <IlVPlatonov@beeline.ru>
2023-02-14 22:57:53 +0300

    refactoring

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -20,25 +20,13 @@ export class CardComponent {
     this.tasksService.deleteTask(cardId);
   }
 
-  public editTask(task: Task): void {
+  public createDialog(task: Task, flag: string, title: string): void {
     this.dialog.open(CreateTaskModalComponent, {
       width: '692px',
       height: '570px',
       data: {
-        title: 'Редактирование задачи',
-        flag: 'edit',
-        task: task,
-      },
-    });
-  }
-
-  public viewTask(task: Task): void {
-    this.dialog.open(CreateTaskModalComponent, {
-      width: '692px',
-      height: '570px',
-      data: {
-        title: 'Просмотр задачи',
-        flag: 'view',
+        title: title,
+        flag: flag,
         task: task,
       },
     });
